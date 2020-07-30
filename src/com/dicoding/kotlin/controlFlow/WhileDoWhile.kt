6aaa01e1650cde060ee3b29d282b89964aca2c61
milson389 <mugetsu3008@gmail.com>
2020-07-30 21:52:39 +0700

    Kotlin While and Do While Loops

---
+++ src/com/dicoding/kotlin/controlFlow/WhileDoWhile.kt
@@ -0,0 +1,21 @@
+package com.dicoding.kotlin.controlFlow
+
+fun main(){
+
+    // While Loops
+    var counter = 1
+    while (counter <= 7){
+        println("Hello World!")
+        counter++
+    }
+
+    println()
+
+    // Do While loops
+    var count = 1
+    do{
+        println("Hello World!")
+        count++
+    }while (count <= 7)
+
+}
